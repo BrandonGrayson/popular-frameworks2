92e63511450510cc372bf4865dade6149e2e4ac8
Brandon <brandonbgrayson@gmail.com>
2021-05-26 16:56:37 -0400

    installed proptypes and set languages to take in selected and onUpdateLanguage

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,20 +1,27 @@
 import { Button, Flex } from "@chakra-ui/react";
 import React from "react";
 import { List, ListItem, ListIcon, OrderedList, UnorderedList } from "@chakra-ui/react"
-
-
-
+// proptypes package
+import PropTypes from "prop-types";
 
 function Languages({ selected, onUpdateLanguage }) {
+
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
+
+
+    languages.propTypes = {
+        selected: PropTypes.string.isRequired,
+        onUpdateLanguage: PropTypes.func.isRequired
+    }
+
     return (
         <List display='flex' flexDirection='row' justifyContent='center' w='100%' h='50%'>
             {languages.map((language) => (
-                <ListItem>
+                <>
                     <Button key={language} style={language === selected ? { color: 'rgb(187, 46, 31) ' } : null} onClick={() => onUpdateLanguage(language)} mr={2} key={language}>
                         {language}
                     </Button>
-                </ListItem>
+                </>
             ))}
         </List>
     )
@@ -39,9 +46,9 @@ export default class Popular extends React.Component {
     }
 
     render() {
-
+        
         const { selectedLanguage } = this.state
-
+        
         return (
             <React.Fragment>
                 <Languages selected={selectedLanguage} onUpdateLanguage={this.updateLanguage} />
